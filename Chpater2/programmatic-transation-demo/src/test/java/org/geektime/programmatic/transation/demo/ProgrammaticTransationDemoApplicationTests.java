727039208bb75ae6819629e4a7a02a0795858a3a
unknown <zhaopengfei@lstcloud.com>
2020-07-28 15:20:15 +0800

    programmatic transation demo

---
+++ Chpater2/programmatic-transation-demo/src/test/java/org/geektime/programmatic/transation/demo/ProgrammaticTransationDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.geektime.programmatic.transation.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgrammaticTransationDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
