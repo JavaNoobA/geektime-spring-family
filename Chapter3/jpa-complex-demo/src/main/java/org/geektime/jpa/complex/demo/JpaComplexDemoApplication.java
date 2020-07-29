ae8d8dbfab4aa47b3e2d3af11491622d2c9cbdc2
zhaopengfei <1027926875@qq.com>
2020-07-29 08:18:31 +0800

    jpa complex demo(todo)

---
+++ Chapter3/jpa-complex-demo/src/main/java/org/geektime/jpa/complex/demo/JpaComplexDemoApplication.java
@@ -0,0 +1,13 @@
+package org.geektime.jpa.complex.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaComplexDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaComplexDemoApplication.class, args);
+    }
+
+}
